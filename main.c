9d6bf786e95ae8a9d1b18dceac03e93fa4c9b4ce
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-07-31 14:07:19 +0200

    struct added

--- main.c
+++ main.c
@@ -3,6 +3,12 @@
 #define WIDTH 1000
 #define HEIGHT 1000
 
+typedef struct {
+	// v[0]=red, v[1]=green, v[2]=blue
+	// Array used to be able to loop on it
+	unsigned char v[3];
+} s_color;
+
 void fillMap(float map[HEIGHT][WIDTH], float min, float max);
 void printMap(float map[HEIGHT][WIDTH], float min, float max);
 void printResult(time_t beginning, time_t end);
