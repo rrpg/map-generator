aa36366cdb8610a9066826914cf419990288df33
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2013-07-31 14:08:26 +0200

    satelite functions added

--- main.c
+++ main.c
@@ -12,6 +12,9 @@ typedef struct {
 void fillMap(float map[HEIGHT][WIDTH], float *min, float *max);
 void printMap(float map[HEIGHT][WIDTH], float min, float max);
 void printResult(time_t beginning, time_t end);
+int fasterFloor(float value);
+float dotproduct(float grad[], float x, float y);
+s_color lerp(s_color c1, s_color c2, float value);
 
 int main()
 {
@@ -45,4 +48,32 @@ void printResult(time_t beginning, time_t end)
 
 }
 
+
+int fasterFloor(float value)
+{
+	return value >= 0 ? (int) value : (int) value - 1;
+}
+
+float dotproduct(float grad[], float x, float y)
+{
+	return grad[0] * x + grad[1] * y;
+}
+
+s_color lerp(s_color c1, s_color c2, float value)
+{
+	s_color tcolor;
+	tcolor.v[0] = tcolor.v[1] = tcolor.v[2] = 0;
+
+	int g;
+	for (g = 0; g < 3; g++) {
+		if (c1.v[g] > c2.v[g]) {
+			tcolor.v[g] = c2.v[g] + (unsigned char) ((float)(c1.v[g] - c2.v[g]) * value);
+		}
+		else {
+			tcolor.v[g] = c1.v[g] + (unsigned char) ((float)(c2.v[g] - c1.v[g]) * value);
+		}
+	}
+
+	return tcolor;
+}
 
